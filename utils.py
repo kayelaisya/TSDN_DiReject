3444b7d72be1648d21ae8e18713de8f71a807742
kayelaisya <aisyakayla0@gmail.com>
2024-11-16 17:16:32 +0700

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+import pandas as pd
+import streamlit as st
+import base64
+import pickle
+import requests
+import io
+
+
+@st.cache(ttl=12500, allow_output_mutation=True)
+def load_model():
+    model = pickle.load(open(r"https://raw.githubusercontent.com/kayelaisya/TSDN_DiReject/main/nb_tsdn.pkl", "rb"))
+    return model
